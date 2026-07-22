bd8b942b574a67edcae88b0afb153aef34cf341a
Your <your.email@example.com>
2026-07-22 14:47:36 +0700

    connect api url deploy

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,4 +1,4 @@
-export const API_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000/api/v1';
+export const API_URL = import.meta.env.VITE_API_BASE_URL || 'https://backend-kasandra.vercel.app/api/v1';
 
 interface ApiFetchOptions extends RequestInit {
   redirectOnUnauthorized?: boolean;
